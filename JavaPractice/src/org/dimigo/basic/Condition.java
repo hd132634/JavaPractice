cfe97a5d2a2481df468c1f846e5f35fcf496b270
하다현 <@dahyeon>
2015-03-26 09:12:39 +0900

    고속도로 통행료 계산

---
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -0,0 +1,32 @@
+package org.dimigo.basic;
+
+public class Condition {
+
+	public static void main(String[] args) {
+		System.out.println("<<고속도로 통행료 계산>>");
+		String car = "고속버스";
+		int s = 10;
+		int a = (s - 10)/10;
+		int m =850 +  a*300;
+		if (s<=10)  {
+		System.out.println("거리 : " + s + "km");
+		System.out.println("차종 : 고속버스");
+		System.out.println("통행료 : 850원");
+		}
+		else if (s>10 && s<=20)  {
+			System.out.println("거리 : " + s + "km");
+			System.out.println("차종 : 고속버스");
+			System.out.println("통행료 : 1150원");
+			}
+		else if (s>20 && s<=30)  {
+			System.out.println("거리 : " + s + "km");
+			System.out.println("차종 : 고속버스");
+			System.out.println("통행료 : 1450원");
+			}
+		else {
+			System.out.println("거리 : " + s + "km");
+			System.out.println("차종 : 고속버스");
+			System.out.println("통행료 : " + m + "원");
+		}
+	}
+}
