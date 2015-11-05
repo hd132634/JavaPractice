0782570c8b8555df69f071fc12ad295399515faa
하다현 <@dahyeon>
2015-11-05 11:46:05 +0900

    thread2

---
+++ JavaPractice/src/org/dimigo/thread/Runner2.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+import java.util.Random;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ * 		|_ Runner
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 11. 5.
+ * </pre>
+ *
+ * @author			 : hd132634
+ * @version			 : 1.0
+ */
+public class Runner2 extends Thread implements Runnable{
+	
+	private String name;
+	
+	public Runner2(String name) {
+		this.name = name;	
+	}
+	
+	public void run() {
+		
+				System.out.println(name + " 출발");
+				System.out.println();
+				
+				for(int i=10; i>=0; i--) {
+					System.out.println(name+ " " + i*10 + " 미터");
+					try {
+						sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+				System.out.println(name + " 골인");
+	}
+}
